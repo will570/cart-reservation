7f4e8e3f83af86869358a615e1ddd9b2817b9888
Claire Xu <claireaxu@g.ucla.edu>
2022-11-05 22:05:27 -0700

    Configure material-ui wrapping and react-router-dom

--- frontend/client/src/pages/reservationPage.js
+++ frontend/client/src/pages/reservationPage.js
@@ -8,59 +8,57 @@ function ReservationPage() {
     */
     return (
         <div className="reservation">
-        <Box component="h1" display="inline" m={3}>
-                    De Neve 
-        </Box>
-        
-        <Box 
-            sx={{
-                component: "fieldset", 
-                m: 3,
-                display: "block",
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-                border: '3px solid grey', }}
-        >
-            <Box justifyContent="space-between" m={3}>
-                <Box component="h2" display="inline">
-                    De Neve 
-                </Box>
-                <Box component="h2" display= "inline" >
-                    : 20 
-                </Box>
+            <Box component="h1" display="inline" m={3}>
+                        De Neve 
             </Box>
-            <Box sx={{ 
-                border: '1px dashed grey',
-                component: "fieldset",
-                m: 3,
-                p: 2,
-                display: "block",
-                justifyContent: "flex-start",
-                alignItems: "flex-start", 
-            }}>  
             
-                <Button variant="contained"
-                    p={2} 
-                    sx={{m: 5, p: 3, backgroundColor: '#ecece4'}}
-                >
-                    Reserve
-                </Button>
-
-                <Box sx={{ 
-                    backgroundColor: '#ecece4',
-                    component: "fieldset",
-                    mt: 3,
-                    p: 2,
+            <Box 
+                sx={{
+                    component: "fieldset", 
+                    m: 3,
                     display: "block",
                     justifyContent: "flex-start",
                     alignItems: "flex-start",
-                }}
-                >  
+                    border: '3px solid grey', }}
+            >
+                <Box justifyContent="space-between" m={3}>
+                    <Box component="h2" display="inline">
+                        De Neve 
+                    </Box>
+                    <Box component="h2" display= "inline" >
+                        : 20 
+                    </Box>
+                </Box>
+                <Box sx={{ 
+                    border: '1px dashed grey',
+                    component: "fieldset",
+                    m: 3,
+                    p: 2,
+                    display: "block",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start", 
+                }}>  
                 
+                    <Button variant="contained"
+                        p={2} 
+                        sx={{m: 5, p: 3, backgroundColor: '#ecece4'}}
+                    >
+                        Reserve
+                    </Button>
+
+                    <Box sx={{ 
+                        backgroundColor: '#ecece4',
+                        component: "fieldset",
+                        mt: 3,
+                        p: 2,
+                        display: "block",
+                        justifyContent: "flex-start",
+                        alignItems: "flex-start",
+                    }}
+                    >  
+                    </Box>
                 </Box>
             </Box>
-
-        </Box>
         </div>
     );
 }
