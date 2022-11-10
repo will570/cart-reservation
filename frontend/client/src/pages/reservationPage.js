8c7ce448515d44f5134a4922dd28a4dd60b89cc4
qluan <george.luan@outlook.com>
2022-11-09 18:07:35 -0800

    Splitted out the building component and added the drawer on the side.

--- frontend/client/src/pages/reservationPage.js
+++ frontend/client/src/pages/reservationPage.js
@@ -1,64 +1,38 @@
 import React from 'react';
 //import button from '../components/button.js';
-import {Button, Box, Grid} from '@material-ui/core';
+import {Drawer, Divider, Box} from '@material-ui/core';
+import Building from '../components/building';
 
 function ReservationPage() {
     /* 
     * This is the skeleton for the reservation page. 
     */
+
     return (
         <div className="reservation">
-            <Box component="h1" display="inline" m={3}>
-                        De Neve 
+            <Box component="h1" display="inline" m={3} pl = {15}>
+                        Reservation 
+            </Box>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 200 },
+                }}
+                open
+                >
+            <Box component="h2" display="inline" m={3}>
+                        Hello 
+            </Box>
+            </Drawer>
+            <Box
+                component="main"
+                justify="flex-start"
+                sx={{ flexGrow: 1, pl: 15}}
+            >
+                <Building/>
             </Box>
             
-            <Box 
-                sx={{
-                    component: "fieldset", 
-                    m: 3,
-                    display: "block",
-                    justifyContent: "flex-start",
-                    alignItems: "flex-start",
-                    border: '3px solid grey', }}
-            >
-                <Box justifyContent="space-between" m={3}>
-                    <Box component="h2" display="inline">
-                        De Neve 
-                    </Box>
-                    <Box component="h2" display= "inline" >
-                        : 20 
-                    </Box>
-                </Box>
-                <Box sx={{ 
-                    border: '1px dashed grey',
-                    component: "fieldset",
-                    m: 3,
-                    p: 2,
-                    display: "block",
-                    justifyContent: "flex-start",
-                    alignItems: "flex-start", 
-                }}>  
-                
-                    <Button variant="contained"
-                        p={2} 
-                        sx={{m: 5, p: 3, backgroundColor: '#ecece4'}}
-                    >
-                        Reserve
-                    </Button>
-
-                    <Box sx={{ 
-                        backgroundColor: '#ecece4',
-                        component: "fieldset",
-                        mt: 3,
-                        p: 2,
-                        display: "block",
-                        justifyContent: "flex-start",
-                        alignItems: "flex-start",
-                    }}
-                    >  
-                    </Box>
-                </Box>
-            </Box>
         </div>
     );
 }
