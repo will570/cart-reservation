634f522b32d4864285efb7ec49c1074d99403f77
Claire Xu <claireaxu@g.ucla.edu>
2022-11-15 10:42:23 -0800

    Fix mui issues, routing for login skeleton page

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import ReservationPage from './pages/ReservationPage';
+import LoginForm from './pages/LoginPage';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Router>
                 <Routes>
                 <Route path="/reservation" element={<ReservationPage />} />
+                <Route path="/login" element={<LoginForm />} />
 
                 </Routes>
             </Router>
--- frontend/client/src/pages/loginPage.js
+++ frontend/client/src/pages/loginPage.js
@@ -14,7 +14,7 @@ function LoginForm({Login, error}){
         <form onSubmit={submitHandler}>
             <div className="form-inner">
                 <h2>Login</h2>
-                {(error != "") ? ( <div className="error">{error}</div>) : ""}
+                {(error !== "") ? ( <div className="error">{error}</div>) : ""}
                 <div className="form-group">
                     <label htmlFor="name">Name:</label>
                     <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
@@ -33,4 +33,4 @@ function LoginForm({Login, error}){
     )
 }
 
-export default LoginForm
+export default LoginForm; 
