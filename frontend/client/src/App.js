b3904c7cdfc32fc35d7bd800ff91c433cdaa058d
Claire Xu <claireaxu@g.ucla.edu>
2022-11-16 15:09:35 -0800

    Implement skeleton sidebar component, standalone

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import ReservationPage from './pages/ReservationPage';
 import LoginForm from './pages/LoginPage';
+import Sidebar from './components/Sidebar'; 
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                 <Route path="/reservation" element={<ReservationPage />} />
                 <Route path="/login" element={<LoginForm />} />
+                <Route path="/sidebar" element={<Sidebar />} />
 
                 </Routes>
             </Router>
