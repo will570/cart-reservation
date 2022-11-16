4b1c81dff0c2dae8afe94abedc896a0c8282b7d5
George Luan <64983832+georgeluan49@users.noreply.github.com>
2022-11-16 10:11:33 -0800

    Add files via upload

---
+++ frontend/client/src/pages/ReservationPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+//import button from '../components/button.js';
+import {Button, Grid, Box, Paper} from '@material-ui/core';
+import Dropdown from '../components/dropdown';
+
+function ReservationPage() {
+    /* 
+    * This is the skeleton for the reservation page. 
+    */
+
+    return (
+        <div className="reservation">
+            <Box component="h1" display="inline" m={3} pl = {15}>
+                        Reservation 
+            </Box>
+            <Grid container spacing={2}>
+                <Grid item xs={2}>
+
+                    <Box component="h2" display="inline" m={3}>
+                                Hello 
+                    </Box>
+                    <Button variant="contained"
+                            shape="round"
+                            p={2} 
+                            sx={{m: 5, p: 3, backgroundColor: '#99dfff'}}
+                    >
+                        Logout
+                    </Button>
+                </Grid>
+            <Grid item xs={8}>
+                <Dropdown/>
+            </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default ReservationPage; 
