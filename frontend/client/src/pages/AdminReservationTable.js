9e46fce32b49e954f42a09a4d086e3c46766d6ea
Guanhua Ji <guanhuaji@g.ucla.edu>
2022-11-18 20:31:41 -0800

    implement admin page

---
+++ frontend/client/src/pages/AdminReservationTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReservationList from '../components/ReservationList'
+import Header from '../components/Header'
+
+function AdminReservationTable(){
+    return (
+        <>
+        <Header />
+        <ReservationList />
+        </>
+    )
+}
+
+export default AdminReservationTable
