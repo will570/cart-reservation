110cf098ad23b4f8f6833c157e1005d83206524b
George Luan <64983832+georgeluan49@users.noreply.github.com>
2022-11-20 08:37:58 -0800

    Update building.js
    Resolve merging confilicts

--- frontend/client/src/components/building.js
+++ frontend/client/src/components/building.js
@@ -6,10 +6,13 @@ function Building(props) {
     /* 
     * This is the building component. 
     */
-    function handleClick(){
-        alert("Reserved!");
-    };
-
+    const { auth } = useAuth();
+    const buildingName = props.name;
+    const handleClick = async (buildingName, uid) => {
+        const { cartID } = await axios.put(`http://localhost:8800/api/reservation/reserveCart/${buildingName}/${uid}`);
+        alert("Successfully reserved the cart at " + buildingName);
+    }
+    
     return (
         <Box width={0.9}>
             <Box 
@@ -25,7 +28,7 @@ function Building(props) {
             >
                 <Box justifyContent="space-between" m={3}>
                     <Box component="h2" display="inline">
-                        {props.name} : {props.n_carts} 
+                        {buildingName} : {props.n_carts} 
                     </Box>
                 </Box>
                 <Box sx={{ 
@@ -41,7 +44,7 @@ function Building(props) {
                     <Button variant="contained"
                         p={2}
                         onClick={() => {
-                            handleClick();
+                            handleClick(buildingName, auth.uid);
                         }} 
                         sx={{m: 5, p: 3, backgroundColor: '#ecece4'}}
                     >
